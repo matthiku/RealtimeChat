b86b49d8ef9dce9974843b59efa36c5f85449abc
Matthias Kuhs <matthiku@yahoo.com>
2017-10-05 15:50:10 +0100

    trying to solve issue with bootstrap v4 dropdown menu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RealtimeChat
+
+- Laravel 5.5
+- Vue.js
+- Bootstrap 4.0
+- Pusher
+- v-toaster for notifications
+- chat stored in session
+
+
+[sample](https://raw.githubusercontent.com/matthiku/RealtimeChat/master/sample.png)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -33,36 +33,30 @@
             <li class="nav-item active">
               <a class="nav-link" href="#">Chat <span class="sr-only">(current)</span></a>
             </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
             @guest
               <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
               <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
             @else
               <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
-
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">Logout
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">Logout
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
                 </div>
               </li>
             @endguest
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
 
